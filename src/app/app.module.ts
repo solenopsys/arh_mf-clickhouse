f2ae3a477038c49cc77851ef199f90b93b0e9762
AlexStorm <info@alexstorm.ru>
2023-03-11 11:35:17 +0530

    fix versions

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,35 +1,38 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
 import {RouterModule, Routes} from '@angular/router';
 import {TABLE_PAGE} from "@solenopsys/ui-templates";
 import {FormsModule} from "@angular/forms";
 import {TABLES} from "./tables.config";
+import {CommonModule} from "@angular/common";
 
 
 const routes: Routes = [
-  TABLE_PAGE(':table'),
+    TABLE_PAGE(':table'),
 ];
 
 export const PROVIDERS_CONF = [
-  {provide: 'tables', useValue: TABLES},
-  {provide: 'assets_dir', useValue: "/fm/modules/mf-richteri/postgres"},
-  {provide: 'mod_name', useValue: "git"}
+    {provide: 'tables', useValue: TABLES},
+    {provide: 'assets_dir', useValue: "/fm/modules/mf-richteri/postgres"},
+    {provide: 'mod_name', useValue: "git"}
 ]
 
 export const IMPORTS_CONF = [
-  BrowserModule,
-  RouterModule.forChild(routes),
-  FormsModule,
+    CommonModule,
+    BrowserModule,
+    RouterModule.forChild(routes),
+    FormsModule,
 ]
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [
-    ...IMPORTS_CONF
-  ],
-  providers: [...PROVIDERS_CONF],
-  bootstrap: [AppComponent],
+    declarations: [AppComponent],
+    imports: [
+        ...IMPORTS_CONF
+    ],
+    providers: [...PROVIDERS_CONF],
+    bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule {
+}
